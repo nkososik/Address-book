87d1fc29b39ecb0a3bb35fb7d3c4a6997ab04e64
Nikolas Kososik <nikolas.kososik@gmail.com>
2020-06-03 16:07:20 -0700

    add form, update radio buttons, and add dropdown menu choices
    Co-Authored-by: Mariel Hamson <mariel.hamson@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+const beverage = $("#beverage").val();
+
+const flavor = $("input:radio[name=flavor]:checked").val();
+
+const dob = $("#born").val();
+
+const favoriteColor = $("#color").val();
+
+$(document).ready(function() {
+  
+}
